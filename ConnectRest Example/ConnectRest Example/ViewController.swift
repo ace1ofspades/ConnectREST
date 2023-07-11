d3e05539705b0cd4c511e486f01b8c2b67eb304f
ace1ofspades <tekinjoseph@gmail.com>
2023-07-12 00:37:33 +0300

    StringExtensions cleared

--- ConnectRest Example/ConnectRest Example/ViewController.swift
+++ ConnectRest Example/ConnectRest Example/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ConnectREST
 
 class ViewController: UIViewController {
 
@@ -16,4 +17,48 @@ class ViewController: UIViewController {
 
 
 }
-
+//
+//class BaseResponse: JsonStruct {
+//    @objc var birthday: String?
+//    @objc var birthday2: String!
+//    var birthday3: Int?
+//    override init(Data data: Data?) {
+//        super.init(Data: data)
+//        birthday2 => parameters["birthday"]
+//        birthday3 => parameters["birthday"]
+//    }
+//}
+//
+//class Test {
+//    init() {
+//        var signUpService = TestService.SignUp()
+//        signUpService.birthday = "Doğum günü"
+//        signUpService.connect { data, response, error in
+//            if let error = error {
+//                // Error Happened
+//            }
+//            if let data = data {
+//                let response = BaseResponse(Data: data)
+//                // Data Arrived
+//            }
+//            if let response = response {
+//                // Response Arrived
+//            }
+//        }
+//    }
+//}
+//
+//class TestService {
+//    class SignUp: BaseConnect {
+//        var username: String?
+//        var fullname: String?
+//        var email: String?
+//        var birthday: String?
+//        var countryCode: Int?
+//        init() {
+//            super.init(Path: "/api/test", Method: .post)
+//            tokenRequired = false
+//        }
+//
+//    }
+//}
