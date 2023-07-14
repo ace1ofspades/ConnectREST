3e2ad7fa33db54fa08e7282b292450be60c98d50
ace1ofspades <tekinjoseph@gmail.com>
2023-07-14 06:21:41 +0300

    123

--- Sources/ConnectREST/Map.swift
+++ Sources/ConnectREST/Map.swift
@@ -20,6 +20,14 @@ public struct Map<T: MapStruct>: Codable {
         self.wrappedValue = wrappedValue
     }
 
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let dictionary = try? container.decode([String: DecodeAny].self) {
+            wrappedValue = T(dictionary)
+        }
+    }
+
     public func encode(to encoder: Encoder) throws {
         var container = try encoder.singleValueContainer()
         try? container.encode(EncodeAny(value?.parameters))
