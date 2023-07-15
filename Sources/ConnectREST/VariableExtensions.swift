0d975a2d1c6940941eeca0ea0e00a553bb9f19d2
ace1ofspades <tekinjoseph@gmail.com>
2023-07-15 04:26:46 +0300

    123123

--- Sources/ConnectREST/VariableExtensions.swift
+++ Sources/ConnectREST/VariableExtensions.swift
@@ -9,14 +9,38 @@ import Foundation
 
 infix operator =>
 
-//public func => <T>(left: inout T?, right: Any?) {
-//    if right is T {
-//        left = right as? T
-//    }
-//}
-//
-//public func => <T:MapStruct>(left: inout T?, right: Any?) {
-//    if right is T {
-//        left = T.init(right)
-//    }
-//}
+public func => <T>(left: inout T?, right: Any?) {
+    if let result = right as? T {
+        left = result
+        return
+    }
+    
+    switch T.self {
+    case is MapStruct.Type:
+        left = (T.self as? MapStruct.Type)?.init(right) as? T
+        break
+    case is Codable.Type:
+        let classType = T.self as! Codable.Type
+        let result = classType => right
+        left = result as? T
+        break
+    default:
+        break
+    }
+}
+
+public func => <T:Codable>(left: T.Type, right: Any?) -> T? {
+    if let object = right as? [String: Any] {
+        if let data = try? JSONSerialization.data(withJSONObject: object),
+           let decoded = try? JSONDecoder().decode(left, from: data) {
+            return decoded
+        }
+    } else if let data = right as? Data {
+        if let decoded = try? JSONDecoder().decode(left, from: data) {
+            return decoded
+        }
+    } else if let result = right as? T {
+        return result
+    }
+    return nil
+}
