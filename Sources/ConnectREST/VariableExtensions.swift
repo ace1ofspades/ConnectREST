23a8972db1395f8b2ca2f88e1d63def9ba5528d6
ace1ofspades <tekinjoseph@gmail.com>
2023-07-15 03:35:50 +0300

    a12

---
+++ Sources/ConnectREST/VariableExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yusuf Tekin on 15.07.2023.
+//
+
+import Foundation
+
+infix operator =>
+
+//public func => <T>(left: inout T?, right: Any?) {
+//    if right is T {
+//        left = right as? T
+//    }
+//}
+//
+//public func => <T:MapStruct>(left: inout T?, right: Any?) {
+//    if right is T {
+//        left = T.init(right)
+//    }
+//}
