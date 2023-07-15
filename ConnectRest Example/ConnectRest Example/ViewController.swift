0a81941c56e1c5a3b8fa4ca29141194fdeebc3d4
ace1ofspades <tekinjoseph@gmail.com>
2023-07-15 08:51:39 +0300

    MapStruct Added

--- ConnectRest Example/ConnectRest Example/ViewController.swift
+++ ConnectRest Example/ConnectRest Example/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        baseURL = "https://example.com"
+        Connect.baseUrl = "https://example.com"
 
     }
 
